58f1c4d68f0a3cd21abd4b07e33e58e8076b2623
Dylan Shine <dylanshinenyc@gmail.com>
2015-08-13 18:09:12 -0400

    Create FuzzDataManager singleton

---
+++ FuzzStagingQuiz/FuzzStagingQuiz/FuzzDataManager.h
@@ -0,0 +1,13 @@
+//
+//  FuzzDataManager.h
+//  FuzzStagingQuiz
+//
+//  Created by Dylan Shine on 8/13/15.
+//  Copyright (c) 2015 Dylan Shine. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FuzzDataManager : NSObject
++(instancetype)sharedManager;
+@end
---
+++ FuzzStagingQuiz/FuzzStagingQuiz/FuzzDataManager.m
@@ -0,0 +1,22 @@
+//
+//  FuzzDataManager.m
+//  FuzzStagingQuiz
+//
+//  Created by Dylan Shine on 8/13/15.
+//  Copyright (c) 2015 Dylan Shine. All rights reserved.
+//
+
+#import "FuzzDataManager.h"
+
+@implementation FuzzDataManager
+
++(instancetype)sharedManager {
+    static FuzzDataManager *_sharedManager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _sharedManager = [[self alloc] init];
+    });
+    return _sharedManager;
+}
+
+@end
