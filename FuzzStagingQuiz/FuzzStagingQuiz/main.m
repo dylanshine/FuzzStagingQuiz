bf47d87a2a43cf013124ff13584a9af1e5f48829
Dylan Shine <dylanshinenyc@gmail.com>
2015-08-13 17:53:44 -0400

    Create project

---
+++ FuzzStagingQuiz/FuzzStagingQuiz/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FuzzStagingQuiz
+//
+//  Created by Dylan Shine on 8/13/15.
+//  Copyright (c) 2015 Dylan Shine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
