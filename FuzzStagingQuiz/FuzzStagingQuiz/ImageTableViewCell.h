fe3a129954a5e1f449f9657f5b32a1c254473d2e
Dylan Shine <dylanshinenyc@gmail.com>
2015-08-13 19:16:33 -0400

    Create custom ImageTableViewCell

---
+++ FuzzStagingQuiz/FuzzStagingQuiz/ImageTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ImageTableViewCell.h
+//  FuzzStagingQuiz
+//
+//  Created by Dylan Shine on 8/13/15.
+//  Copyright (c) 2015 Dylan Shine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageTableViewCell : UITableViewCell
+
+@end
---
+++ FuzzStagingQuiz/FuzzStagingQuiz/ImageTableViewCell.m
@@ -0,0 +1,18 @@
+//
+//  ImageTableViewCell.m
+//  FuzzStagingQuiz
+//
+//  Created by Dylan Shine on 8/13/15.
+//  Copyright (c) 2015 Dylan Shine. All rights reserved.
+//
+
+#import "ImageTableViewCell.h"
+
+@interface ImageTableViewCell ()
+@property (weak, nonatomic) IBOutlet UIImageView *fuzzImageView;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+@end
+
+@implementation ImageTableViewCell
+
+@end
