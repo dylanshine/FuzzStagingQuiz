7247ec0ed9145e4980e9991c5facedff6d805979
Dylan Shine <dylanshinenyc@gmail.com>
2015-08-13 18:52:04 -0400

    Implement setupFuzzDataManager in main view controller

--- FuzzStagingQuiz/FuzzStagingQuiz/ViewController.m
+++ FuzzStagingQuiz/FuzzStagingQuiz/ViewController.m
@@ -8,17 +8,27 @@
 
 #import "ViewController.h"
 #import "Constants.h"
+#import "FuzzDataManager.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) FuzzDataManager *fuzzDataManager;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self setupFuzzDataManager];
    
 }
 
+#pragma mark - Setup View Controller
+
+-(void) setupFuzzDataManager {
+    self.fuzzDataManager = [FuzzDataManager sharedManager];
+    [self.fuzzDataManager fetchFuzzData:kFUZZ_API completion:^{
+    
+    }];
+}
 
 @end
