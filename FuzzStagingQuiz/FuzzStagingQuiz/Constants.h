e5bb09b0ce27c2b0c5cc9a4842a102e051746790
Dylan Shine <dylanshinenyc@gmail.com>
2015-08-13 18:02:22 -0400

    Add constants file

---
+++ FuzzStagingQuiz/FuzzStagingQuiz/Constants.h
@@ -0,0 +1,11 @@
+//
+//  Constants.h
+//  FuzzStagingQuiz
+//
+//  Created by Dylan Shine on 8/13/15.
+//  Copyright (c) 2015 Dylan Shine. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString *const kFUZZ_API;
