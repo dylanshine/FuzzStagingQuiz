90dc9df0d5d2456b3352bccce0e0abd14fa31a4c
Dylan Shine <dylanshinenyc@gmail.com>
2015-08-13 19:25:45 -0400

    Create TextTableViewCell

---
+++ FuzzStagingQuiz/FuzzStagingQuiz/TextTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TextTableViewCell.h
+//  FuzzStagingQuiz
+//
+//  Created by Dylan Shine on 8/13/15.
+//  Copyright (c) 2015 Dylan Shine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextTableViewCell : UITableViewCell
+
+@end
---
+++ FuzzStagingQuiz/FuzzStagingQuiz/TextTableViewCell.m
@@ -0,0 +1,18 @@
+//
+//  TextTableViewCell.m
+//  FuzzStagingQuiz
+//
+//  Created by Dylan Shine on 8/13/15.
+//  Copyright (c) 2015 Dylan Shine. All rights reserved.
+//
+
+#import "TextTableViewCell.h"
+
+@interface TextTableViewCell ()
+@property (weak, nonatomic) IBOutlet UILabel *fuzzTextLabel;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+@end
+
+@implementation TextTableViewCell
+
+@end
