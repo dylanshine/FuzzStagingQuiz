a56ecd1aa30ba9a96e3c618d2e06f5e3182fd48b
Dylan Shine <dylanshinenyc@gmail.com>
2015-08-13 20:33:09 -0400

    Add spinner to ImageTableViewCell for loading image

--- FuzzStagingQuiz/FuzzStagingQuiz/ImageTableViewCell.m
+++ FuzzStagingQuiz/FuzzStagingQuiz/ImageTableViewCell.m
@@ -12,6 +12,7 @@
 @interface ImageTableViewCell ()
 @property (weak, nonatomic) IBOutlet UIImageView *fuzzImageView;
 @property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
 @end
 
 @implementation ImageTableViewCell
@@ -20,14 +21,18 @@
     [super prepareForReuse];
     [self.fuzzImageView cancelImageRequestOperation];
     self.fuzzImageView.image = nil;
+    [self.spinner stopAnimating];
 }
 
 -(void) setImageData:(FuzzImageData *)imageData {
     _imageData = imageData;
+    [self.spinner startAnimating];
     NSURLRequest *request = [NSURLRequest requestWithURL:imageData.imageURL cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:6];
     [self.fuzzImageView setImageWithURLRequest:request placeholderImage:nil success:^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *image) {
+        [self.spinner stopAnimating];
         self.fuzzImageView.image = image;
     } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error) {
+        [self.spinner stopAnimating];
         UIImage *unavailableImage = [UIImage imageNamed:@"unavailable"];
         self.fuzzImageView.image = unavailableImage;
     }];
