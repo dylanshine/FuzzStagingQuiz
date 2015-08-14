b865bee34de970c0d4eebd476846346117783175
Dylan Shine <dylanshinenyc@gmail.com>
2015-08-13 20:36:15 -0400

    Configure view controller to display & reload data

--- FuzzStagingQuiz/FuzzStagingQuiz/ViewController.m
+++ FuzzStagingQuiz/FuzzStagingQuiz/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 #import "Constants.h"
 #import "FuzzDataManager.h"
+#import "ImageTableViewCell.h"
+#import "TextTableViewCell.h"
+#import "FuzzImageData.h"
+#import "FuzzTextData.h"
 
 @interface ViewController () <UITableViewDelegate, UITableViewDataSource>
 @property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
@@ -23,7 +27,6 @@
     [super viewDidLoad];
     [self setupFuzzDataManager];
     [self setupTableView];
-   
 }
 
 #pragma mark - Setup View Controller
@@ -31,19 +34,33 @@
 -(void) setupFuzzDataManager {
     self.fuzzDataManager = [FuzzDataManager sharedManager];
     [self.fuzzDataManager fetchFuzzData:kFUZZ_API completion:^{
-    
+        [self reloadFuzzData];
     }];
 }
 
 -(void) setupTableView {
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
+    self.tableView.estimatedRowHeight = 150.0;
+    self.tableView.rowHeight = UITableViewAutomaticDimension;
 }
 
 #pragma mark - Segmeneted Control
 
 - (IBAction)segmentedControlTapped:(id)sender {
-    
+    [self reloadFuzzData];
+}
+
+-(void)reloadFuzzData {
+    NSInteger selectedSegment = self.segmentedControl.selectedSegmentIndex;
+    if (selectedSegment == 0) {
+        self.displayedData = [self.fuzzDataManager allData];
+    } else if (selectedSegment == 1) {
+        self.displayedData = [self.fuzzDataManager imageData];
+    } else {
+        self.displayedData = [self.fuzzDataManager textData];
+    }
+    [self.tableView reloadData];
 }
 
 #pragma mark - Tableview Delegate & Data Source
@@ -53,8 +70,18 @@
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    UITableViewCell *cell = [[UITableViewCell alloc] init];
-    return cell;
+    
+    if ([self.displayedData[indexPath.row]isKindOfClass:[FuzzImageData class]]) {
+        FuzzImageData *fuzzImage = self.displayedData[indexPath.row];
+        ImageTableViewCell *cell = (ImageTableViewCell *)[tableView dequeueReusableCellWithIdentifier:@"imageCell" forIndexPath:indexPath];
+        cell.imageData = fuzzImage;
+        return cell;
+    } else {
+        FuzzTextData *fuzzText = self.displayedData[indexPath.row];
+        TextTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"textCell" forIndexPath:indexPath];
+        cell.textData = fuzzText;
+        return cell;
+    }
 }
 
 @end
